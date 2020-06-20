7a53d488352b0d7fefcb3f048dd7c8f3a37e340e
Jimmy <dom.jimmy@gmail.com>
2020-06-20 18:50:18 +0200

    Create appImage build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+qmake Hardware-Monitor-Applet.pro
+
+# build project and install files into AppDir
+make -j$(nproc)
+make install INSTALL_ROOT=AppDir
+
+
+
+# now, build AppImage using linuxdeploy and linuxdeploy-plugin-qt
+# download linuxdeploy and its Qt plugin
+wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+wget https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
